736f295a4a8eede40181349bbe2ed2fb49ae0ba8
Sinali Dionou <sinaloid@gmail.com>
2023-08-14 19:53:07 +0000

    logo

--- resources/js/src/components/imgs/Logo.jsx
+++ resources/js/src/components/imgs/Logo.jsx
@@ -1,10 +1,11 @@
 
 import logo from "../../assets/imgs/logo.png"
+import { URL_ } from "../../services/request"
 
 const Logo = () => {
 
     return (
-        <span><img className="img" width="81px" src={logo} alt="" /></span>
+        <span><img className="img" width="81px" src={URL_+"images/logo.png"} alt="" /></span>
     )
 
 }
